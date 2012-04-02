6c2fb89bbf65762779fe800714dde579298f7eb9
Elio Maldonado <emaldona@redhat.com>
2012-04-02 11:11:22 -0700

    Remove the scripts as it get's inclued in the archive. There may be a way around such using mozilla instead of HEAD.

--- tarpem4nss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Creates the source tar ball for use by nss.spec
-# The archive is added to git lookaside cache for nss
-
-git archive --format=tar HEAD | bzip2 -c > nss-pem-$(date +%Y%m%d).tar.bz2
